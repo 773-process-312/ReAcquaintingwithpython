829d498d479b3ebeddaef41e68f3dbbeeea7ed6c
NICO SMITH <nicosmith312@gmail.com>
2022-11-20 11:33:01 -0500

    changes for class

---
+++ BasicStrings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+#we are working on strings and numbers aka str and int
+#make the information a comment so when i see a pound sign it means its a code comment or non runable information
+
+#variables are A B X for this exercise please assign values to the variables
+
+A = 55 # A is a type of int (int is short for integer)
+B = "Tony Montana" # B is a type of str (str is short for string)
+X = "2300 Jackson Street" # X is a type of str
+
+print(X)
+print(A)
+print(B)
+
+#greatsunday 11/20/22
