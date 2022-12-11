c56a34f37067080d4ae5c13e266d30b1dd4e095d
NICO SMITH <nicosmith312@gmail.com>
2022-12-11 13:49:55 -0500

    yes pandas

--- pandastuts/pandas01.py
+++ pandastuts/pandas01.py
@@ -2,5 +2,5 @@
 
 import pandas as pd
 
-df = pd.read_csv('pandastuts\MOCK_DATAImproved.csv')
+df = pd.read_csv('C:/Users/nicol/Documents/0B_SundayPythonTraining/ReAcquaintingwithpython/ReAcquaintingwithpython/pandastuts/MOCK_DATAImproved.csv')
 print(df)
---
+++ pandastuts/pandas2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import pandas as pd
+
+df = pd.read_excel('C:/Users/nicol/Documents/0B_SundayPythonTraining/ReAcquaintingwithpython/ReAcquaintingwithpython/pandastuts/MOCK_DATAImproved.xlsx')
+print(df)
